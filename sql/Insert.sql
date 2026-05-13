20f79cf70ccf2aabdbe4009ffa7b87cb76715a0d
Carmen Pérez <cperezrobles@gmail.com>
2026-05-13 13:30:56 +0200

    Create Insert.sql

---
+++ sql/Insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO FormCarmenPerez (name, email, subject, age, message) values
+('ana', 'ana@gmail.com', 'information', NULL, 'hola testing'),
+(NULL, 	'pedro@gmail.com', 'job', 20, 'pedro mensaje'),
+('maria', 'maria@yahoo.com', 'other', 25, 'mensaje de prueba'),
+('ana', 'ana.work@gmail.com', 'job', 30, 'test message'),
+(NULL, 'user1@gmail.com', 'information', 20, 'mensaje testing');
