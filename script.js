8a840676d337c9af48475d998c138887c280410b
Carmen Pérez <cperezrobles@gmail.com>
2026-05-18 11:18:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // 1. CAMBIAR POR URL Y KEY DE TU API
-const SUPABASE_URL = 'https://iwdierxmgildaqzycppj.supabase.co/rest/v1/formcarmenperez';
+SUPABASE_URL = 'https://iwdierxmgildaqzycppj.supabase.co/rest/v1/formcarmenperez';
 >>>>>>> 8a800557eaabfbfe0da91e3b1e94dcfdb4687428
-const SUPABASE_API_KEY = 'sb_publishable_IN6V-2TZs7Ii5aEC8e85Qw_Nvc2rr3V';
+SUPABASE_API_KEY = 'sb_publishable_IN6V-2TZs7Ii5aEC8e85Qw_Nvc2rr3V';
 
 
 //ENVIAR DATOS DE UN FORMULARIO A UNA API CON POST
