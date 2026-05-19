740d7d5e4531c3cd8a30394890ce1873b02e8d4a
Carmen Pérez <cperezrobles@gmail.com>
2026-05-19 19:42:50 +0200

    Update update.sql

--- sql/update.sql
+++ sql/update.sql
@@ -7,11 +7,3 @@ WHERE email = 'maria@yahoo.com';
 UPDATE formcarmenperez
 SET age= 21
 WHERE age= 20
-
--- Eliminar todos los registros con mensaje que contenga test
-DELETE FROM formcarmenperez
-WHERE message LIKE '%test%';
-
--- Eliminar todos los registros con email pedro@gmail.com
-DELETE FROM formcarmenperez
-WHERE email = 'pedro@gmail.com';
